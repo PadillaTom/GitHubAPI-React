55a43f6684308d33917bcd4d0a1fa9ca074e9929
Tomas Padilla <padillatomasagustin@gmail.com>
2021-05-27 08:33:35 +0200

    UsersInfo Cards

--- src/Components/User Related/Info.js
+++ src/Components/User Related/Info.js
@@ -3,14 +3,73 @@ import styled from 'styled-components';
 
 import {GithubContext} from "../../Context/userContext"
 
+import {GoRepo, GoGist} from "react-icons/go";
+import {FiUsers, FiUserPlus} from "react-icons/fi";
+
 
 const UserInfo = () => {
   // Context:
-  const data = useContext(GithubContext);
-  console.log(data);
+  const {githubUser} = useContext(GithubContext);
+  const {public_repos, followers, following, public_gists} = githubUser;
 
-  return <h2>user info component</h2>;
+  // Data for UserInfo Cards:
+  const myItems = [
+    {
+      id: 1,
+      icon: <GoRepo className="icon"></GoRepo>,
+      label: "Repos",
+      value: public_repos,
+      color: "pink",
+    },
+    {
+      id: 2,
+      icon: <FiUsers className="icon"></FiUsers>,
+      label: "Followers",
+      value: followers,
+      color: "green",
+    },
+    {
+      id: 3,
+      icon: <FiUserPlus className="icon"></FiUserPlus>,
+      label: "Following",
+      value: following,
+      color: "purple",
+    },
+    {
+      id: 4,
+      icon: <GoGist className="icon"></GoGist>,
+      label: "Gists",
+      value: public_gists,
+      color: "yellow",
+    },
+  ]
+  
+  // *** Main ***
+  return <section className="section">
+    <Wrapper className="section-center">
+      {myItems.map((item)=>{
+        return <Item key={item.id} {...item}></Item>
+      })}
+    </Wrapper>
+  </section>;
 };
+
+// *** Component that displays each card:
+const Item = ({icon, label, value, color}) => {
+  return (
+    <article className="item">
+      <span className={color}>
+        {icon}
+      </span>
+      <div>
+        <h3>{value}</h3>
+        <p>{label}</p>        
+      </div>
+    </article>
+  )
+}
+
+
 
 const Wrapper = styled.section`
   display: grid;
